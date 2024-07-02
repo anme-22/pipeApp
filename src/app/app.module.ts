f0c393392a567b2cb4c10f67b9e458ec8632fe54
anme-22 <angeldavidmendez2012@gmail.com>
2024-07-02 16:13:06 -0600

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, SharedModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
